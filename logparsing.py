32f14ba9062d1bb3b12ba422061751018a0faf33
mkevans1220 <119643022+mkevans1220@users.noreply.github.com>
2023-04-13 23:45:17 -0400

    Practice Log Parsing
    I used an dns.log given in an attachment from a practice question from Fullstack Academys' Student Canvas , to write a script that parses that log file and gives me every Ip within the dns.log file. The script also only adds Ips to the "Every_ip" list once to avoid repeatedly adding the same Ip's to said list

---
+++ logparsing.py
@@ -0,0 +1,14 @@
+#fullstack practice activity dns log 
+
+
+#Printing a list of all Ips contained in the dns.log file
+Every_ip = []
+with open("dns.log","r") as f:
+    var = f.readline()
+    my_list = var.split()
+    print(my_list[1])
+    for l in f:
+        Ip = l.split()[1]
+        if Ip not in Every_ip:
+            Every_ip.append(Ip)
+print(Every_ip)
